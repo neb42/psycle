8b412d763ad0585c7de711aba0df8f8524d390b3
Ben McAlindin <bmcalindin@asidatascience.com>
2019-07-12 10:53:03 +0100

    Add tooltips

--- src/components/Visualisations/WeekScatter/WeekScatter.js
+++ src/components/Visualisations/WeekScatter/WeekScatter.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { selectAll } from 'd3-selection';
+import { selectAll, select } from 'd3-selection';
 import { transition } from 'd3-transition';
 import styled from 'styled-components';
+import moment from 'moment';
 
 const R = styled.rect`
   & + text {
@@ -105,26 +106,6 @@ export default class WeekScatter extends React.PureComponent {
 
     return (
       <React.Fragment>
-        {weeklyCount.map((datum, i) => (
-          <React.Fragment>
-            <line
-              className="week-line"
-              x1={xScatterScale(this.timeInteger(datum.key))}
-              x2={xScatterScale(this.timeInteger(datum.key))}
-              y1={yScatterScale(0)}
-              y2={visible ? yScatterScale(datum.value) + this.yOffset : yScatterScale(0)}
-              stroke="#fff"
-            />
-            <circle
-              className="week-scatter-point"
-              cx={xScatterScale(this.timeInteger(datum.key))}
-              cy={visible ? yScatterScale(datum.value) + this.yOffset : height}
-              r={Math.min(5, this.rectHeight)}
-              fillOpacity={visible ? 1 : 0}
-              fill={'#fff'}
-            />
-          </React.Fragment>
-        ))}
         <g>
           {range(yScatterScale.domain()[1], yScatterScale.domain()[0]).slice(1).reverse().map((v, i) => (
             <React.Fragment>
@@ -137,19 +118,94 @@ export default class WeekScatter extends React.PureComponent {
                 fill="#fff"
               />
               <text
+                className={`y-axis-text-${v}`}
                 x={20}
                 y={(i * this.rectHeight) + 50 + this.yOffset + 8}
                 fill="white"
                 style={{
                   fontSize: 16,
                   fontFamily: 'soin_sans_neueroman,sans-serif',
-                  fillOpacity: 0.8,
+                  fillOpacity: 0.6,
                 }}
               >
                 {v}
               </text>
             </React.Fragment>
           ))}
+        </g>
+        {weeklyCount.map((datum, i) => (
+          <React.Fragment>
+            <line
+              className="week-line"
+              x1={xScatterScale(this.timeInteger(datum.key))}
+              x2={xScatterScale(this.timeInteger(datum.key))}
+              y1={yScatterScale(0)}
+              y2={visible ? yScatterScale(datum.value) + this.yOffset + Math.min(5, this.rectHeight) : yScatterScale(0)}
+              stroke="#fff"
+            />
+            <circle
+              className={`week-scatter-point circle-${datum.key}`}
+              cx={xScatterScale(this.timeInteger(datum.key))}
+              cy={visible ? yScatterScale(datum.value) + this.yOffset : height}
+              r={Math.min(5, this.rectHeight)}
+              fillOpacity={visible ? 1 : 0}
+              onMouseEnter={() => {
+                const w = 100;
+                const h = 60;
+                const x = this.timeInteger(datum.key) > 3 * 1440 ? 50 : 400;
+                const y = yScatterScale(datum.value) - h / 2;
+
+                const [weekday, hour, minute] = datum.key.split('-');
+
+                select('.tooltip-rect').transition(transition('a').duration(300)).attr('opacity', 0.6);
+                select('.tooltip-rect').transition(transition().duration(0)).attr('x', x).attr('y', y).attr('width', w).attr('height', h);
+                selectAll('.tooltip-weekday, .tooltip-time').transition(transition('aa').duration(300)).style('fill-opacity', 1);
+                select('.tooltip-weekday').transition(transition().duration(0)).attr('x', x + w / 2).attr('y', y + 20).text(moment().day(Number(weekday)).format('dddd'));
+                select('.tooltip-time').transition(transition().duration(0)).attr('x', x + w / 2).attr('y', y + h - 20).text(moment().hour(Number(hour)).minute(Number(minute)).format('h:mma'));
+
+                select(`.y-axis-text-${datum.value}`).transition(transition().duration(300)).style('fill-opacity', 1).style('opacity', 1);
+                selectAll('.week-line, .week-scatter-point').filter(d => d.key !== datum.key).transition(transition().duration(300)).attr('opacity', 0.3);
+              }}
+              onMouseLeave={() => {
+                select('.tooltip-rect').transition(transition('a').duration(300)).attr('opacity', 0);
+                select('.tooltip-rect').transition(transition().duration(0).delay(300)).attr('x', 0).attr('y', 0).attr('width', 0).attr('height', 0);
+                selectAll('.tooltip-weekday, .tooltip-time').transition(transition('aa').duration(300)).style('fill-opacity', 0);
+                select('.tooltip-weekday').transition(transition().duration(0).delay(300)).attr('x', 0).attr('y', 0).text('');
+                select('.tooltip-time').transition(transition().duration(0).delay(300)).attr('x', 0).attr('y', 0).text('');
+
+                select(`.y-axis-text-${datum.value}`).transition(transition().duration(300)).attr('fill-opacity', 0.6).style('opacity', null);
+                selectAll('.week-line, .week-scatter-point').filter(d => d.key !== datum.key).transition(transition('aa').duration(300)).attr('opacity', 1);
+              }}
+              fill={'#fff'}
+            />
+          </React.Fragment>
+        ))}
+        <g>
+          <rect
+            className="tooltip tooltip-rect"
+            fill="#1a1a1a"
+            opacity={0}
+          />
+          <text
+            className="tooltip tooltip-weekday"
+            textAnchor="middle"
+            alignmentBaseline="central"
+            fill="#fff"
+            style={{
+              fillOpacity: 0,
+              fontFamily: 'soin_sans_neueroman,sans-serif',
+            }}
+          />
+          <text
+            className="tooltip tooltip-time"
+            textAnchor="middle"
+            alignmentBaseline="central"
+            fill="#fff"
+            style={{
+              fillOpacity: 0,
+              fontFamily: 'soin_sans_neueroman,sans-serif',
+            }}
+           />
         </g>
       </React.Fragment>
     );
