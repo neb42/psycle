5faafab0c7986f58082492c46f8c057521757346
Ben McAlindin <bmcalindin@asidatascience.com>
2019-07-17 17:05:57 +0100

    Add linting packages

--- package-scripts.js
+++ package-scripts.js
@@ -1,6 +1,6 @@
 const {
   series: { nps: series },
-  // concurrent: { nps: parallel },
+  concurrent: { nps: parallel },
 } = require('nps-utils');
 
 module.exports = {
@@ -21,6 +21,30 @@ module.exports = {
       },
       updateLegacy: {
         script: 'yarn add @asidatascience/adler-ui',
+        description: '',
+      },
+    },
+    lint: {
+      default: {
+        script: parallel('lint.flow', 'lint.js'),
+        description: '',
+      },
+      flow: {
+        script: 'flow',
+        description: 'Run flow',
+      },
+      js: {
+        default: {
+          script: 'eslint src',
+          description: 'Run eslint',
+        },
+        fix: {
+          script: 'nps "lint.js --fix"',
+          description: 'Run eslint and fix issues',
+        },
+      },
+      css: {
+        script: 'stylelint src/**/*.styles.js',
         description: '',
       },
     },
