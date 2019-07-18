3e3425fb25cbf673542fd0514f4cb592b02e5320
Ben McAlindin <bmcalindin@asidatascience.com>
2019-07-18 13:18:13 +0100

    Handle empty data

--- src/Data/favouriteInstructor.js
+++ src/Data/favouriteInstructor.js
@@ -1,11 +1,11 @@
 const favouriteInstructorName = bookingHistory => {
-  return bookingHistory
-    .sort(
-      (a, b) =>
-        bookingHistory.filter(v => v.instructor === a.instructor).length -
-        bookingHistory.filter(v => v.instructor === b.instructor).length,
-    )
-    [bookingHistory.length - 1].instructor.toLowerCase();
+  const favouriteInstructor = bookingHistory.sort(
+    (a, b) =>
+      bookingHistory.filter(v => v.instructor === a.instructor).length -
+      bookingHistory.filter(v => v.instructor === b.instructor).length,
+  )[bookingHistory.length - 1]
+  
+  return favouriteInstructor ? favouriteInstructor.instructor.toLowerCase() : '';
 };
 
 const data = ({ bookingHistory, width, height }) => ({
